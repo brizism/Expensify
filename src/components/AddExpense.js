4470292f52143754f69a82a35dced191dc2cfa5b
brizism <montielbriseida@gmail.com>
2018-01-26 13:47:40 -0500

    connect AddExpense to Redux store and dispatch the obj from onSubmit

--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
+import { addExpense } from '../actions/expenses'
 
-const AddExpense = () => (
+
+// now we have access to props.dispatch 
+const AddExpense = (props) => (
   <div>
     <h1>Add Expense</h1>
-    <ExpenseForm />
+    <ExpenseForm 
+    // passing onSubmit as a prop 
+      onSubmit={(expense) => {
+        // dispatching our action addExpense (imported above) with the expense obj
+        props.dispatch(addExpense(expense));
+        props.history.push('/');
+      }}
+    />
   </div>
 )
 
-export default AddExpense;
+
+// connecting AddExpense to the store so it cana dispatch
+export default connect()(AddExpense);
