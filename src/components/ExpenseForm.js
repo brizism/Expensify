a9be80474db10a99f055ec4457433dcd3c61c207
brizism <montielbriseida@gmail.com>
2018-01-26 13:45:33 -0500

    onSubmit handler with validation

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -16,7 +16,8 @@ export default class ExpenseForm extends React.Component {
     note: '',
     amount: '',
     createdAt: moment(),
-    calendarFocused: false
+    calendarFocused: false,
+    error: ''
   };
   onDescriptionChange = (e) => {
     const description = e.target.value;
@@ -29,20 +30,44 @@ export default class ExpenseForm extends React.Component {
   onAmountChange = (e) => {
     const amount = e.target.value;
 
-    if(amount.match(/^\d*(\.\d{0,2})?$/)) {
+    // this regex no longer matches an empty string
+    // if the isn't amount or a match THEN set it
+    if(!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
       this.setState(() => ({ amount }))
     }
   };
   onDateChange = (createdAt) => {
-    this.setState(() => ({ createdAt }))
+    // only if there is a credatedAt
+    // preventing user from clearing value
+    if(createdAt) {
+      this.setState(() => ({ createdAt }))
+    }
   };
   onFocusChange = ({ focused }) => {
     this.setState(() => ({ calendarFocused: focused }))
   };
+  onSubmit = (e) => {
+    e.preventDefault();
+    if(!this.state.description || !this.state.amount) {
+      this.setState(() => ({ error: 'Please provide description and amount.' }))
+    } else {
+      this.setState(() => ({ error: '' }))
+
+      // calling prop onSubmit
+      this.props.onSubmit({
+        description: this.state.description,
+        amount: parseFloat(this.state.amount, 10) * 100,
+        // valueOf() -> Unix timestamp millisec
+        createdAt: this.state.createdAt.valueOf(),
+        note: this.state.note
+      });
+    }
+  };
   render(){
     return(
       <div>
-        <form>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.onSubmit}>
           <input
             type="text"
             placeholder="Description"
